8058eb737e8080298cde41b29181cfd506f6455d
Jigme Lodey <jigmenlodey@gmail.com>
2025-03-26 20:35:00 +0600

    feat: course 3

---
+++ src/db/migrations/0001_harsh_dagger.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "customers" ADD COLUMN "address1" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "customers" DROP COLUMN "address";
---
+++ src/db/migrations/0002_strong_shooting_star.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "customers" RENAME COLUMN "note" TO "notes";
